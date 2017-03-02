858a1f817963b9c345d17b5bf14d5c3402194bd5
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-03-01 20:40:11 -0500

    Adding new session code

--- lambda/index.js
+++ lambda/index.js
@@ -14,13 +14,51 @@ exports.handler = function(event, context, callback){
 
 var handlers = {
 
-  'LaunchRequest': function () {
-    this.emit(':ask', 'Welcome to Voice Devs!', 'Try saying hello!');
-  },
+ 'NewSession': function () {
+   this.emit(':ask', `Welcome to Voice Devs, The skill that gives you information about the alexa developer community.
+   You can ask me about the various alexa meetups around the world, or listen to the Alexa Dev Chat podcast.
+   But first I\'d like to get to know you better.
+   Tell me your name by saying: My name is, and then your name.`, 'Tell me your name by saying: My name is, and then your name.');
+ },
 
-  'Hello': function () {
-    this.emit(':tell', 'Hi there!');
-  },
+ 'NameCapture': function () {
+   // Get Slot Values
+   var USFirstNameSlot = this.event.request.intent.slots.USFirstName.value;
+   var UKFirstNameSlot = this.event.request.intent.slots.UKFirstName.value;
+
+   //Get NameCapture
+   var name;
+   if (USFirstNameSlot) {
+     name = USFirstNameSlot;
+   } else if (UKFirstNameSlot) {
+     name = UKFirstNameSlot
+   }
+
+   //Save name in Session Attributes and Ask for Country
+   if (name) {
+     this.attributes['userName'] = name;
+     this.emit(':ask', `Ok, ${name}! Tell me what country you're from by saying: I'm from, and then the country you're from.`, `Tell me what country you're from by saying: I'm from, and then the country you're from.`);
+   } else {
+     this.emit(':ask', `Sorry I didn't recognize that name. Please tell me your name by saying: My name is, and then your name.`, `Please tell me your name by saying: My name is, and then your name.`);
+   }
+  //  this.emit(':ask', ``);
+ },
+
+ 'CountryCapture': function () {
+   //Get slot Values
+   var country = this.event.request.intent.slots.Country.value;
+   //Get user name from session attributes
+   var userName = this.attributes['userName'];
+
+   // Save Country Name in Session Attributes and Ask for favorite programming language
+   if (country) {
+     this.attributes['userCountry'] = country;
+     this.emit(':ask', `Ok ${userName}! Your from ${country}, that's great! You can ask me about the various alexa meetups around the world, or listen to the Alexa dev chat podcast.
+     What would you like to do?`, 'What would you like to do');
+   } else {
+     this.emit(':ask', `Sorry I didn't recognize that country. Please tell me what country you're from by saying: I am from and then your country.`, `Please tell me what country you're from by saying: I am from and then your country.`);
+   }
+ },
 
  'AlexaMeetupNumbers': function () {
    var meetupNumbers = alexaMeetups.length;
